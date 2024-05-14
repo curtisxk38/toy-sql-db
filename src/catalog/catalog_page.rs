fd1c3fed9843f24f8cab0e769d63395568d4ec99
curtisxk38 <curtisxk38@scribe.33mail.com>
2024-05-14 19:38:26 -0400

    add first page id of the table to table schema

--- src/catalog/catalog_page.rs
+++ src/catalog/catalog_page.rs
@@ -34,16 +34,17 @@ impl Column {
 
 struct TableSchema {
     pub name: String,
+    pub first_page_id: u32,
     pub columns: Vec<Column>
 }
 
 impl TableSchema {
-    fn new(name: String, columns: Vec<Column>) -> TableSchema {
-        TableSchema {name, columns}
+    fn new(name: String, columns: Vec<Column>, first_page_id: u32) -> TableSchema {
+        TableSchema {name, first_page_id, columns}
     }
     fn serialize(&self) -> Vec<u8> {
         // layout in bytes
-        // [length of name] [name] [number of columns] [column entries]+
+        // [length of name] [name] [page id of first page] [number of columns] [column entries]+
         // where each column entry is [column type][column name length][column name]
         let mut res = Vec::new();
 
@@ -52,6 +53,8 @@ impl TableSchema {
 
         res.push(name_length);
         res.extend(self.name.as_bytes());
+
+        res.extend(self.first_page_id.to_le_bytes());
 
         let num_columns = u8::try_from(self.columns.len()).unwrap();
         res.push(num_columns);
@@ -68,29 +71,40 @@ impl TableSchema {
 
     fn deserialize(data: Vec<u8>) -> TableSchema {
         // see serialize
+        let mut bytes_read = 0;
+        
         let name_len = usize::from(data[0]);
-        let name = &data[1..name_len+1];
+        bytes_read += 1;
+
+        let name = &data[bytes_read..bytes_read+name_len];
         let name: String = str::from_utf8(name).unwrap().to_string();
-        let num_col = usize::from(data[name_len+1]);
+        bytes_read += name_len;
+
+        let first_page_id = &data[bytes_read..bytes_read+4];
+        let first_page_id = u32::from_le_bytes(first_page_id.try_into().unwrap());
+        bytes_read += 4;
+
+        let num_col = usize::from(data[bytes_read]);
+        bytes_read += 1;
+
         let mut columns = Vec::new();
 
-        let mut reading_from = name_len+2;
         loop {
             if columns.len() == num_col {
                 break;
             }
-            let col_type = decode_column_type(data[reading_from]);
-            reading_from += 1;
-            let col_name_len = usize::from(data[reading_from]);
-            reading_from += 1;
-            let col_name = str::from_utf8(&data[reading_from..reading_from+col_name_len]).unwrap().to_string();
-            reading_from += col_name_len;
+            let col_type = decode_column_type(data[bytes_read]);
+            bytes_read += 1;
+            let col_name_len = usize::from(data[bytes_read]);
+            bytes_read += 1;
+            let col_name = str::from_utf8(&data[bytes_read..bytes_read+col_name_len]).unwrap().to_string();
+            bytes_read += col_name_len;
 
             columns.push(Column::new(col_name, col_type));
         }
 
 
-        TableSchema::new(name, columns)
+        TableSchema::new(name, columns, first_page_id)
     }
 }
 
@@ -102,11 +116,12 @@ mod tests {
     #[test]
     fn simple_serialize() {
         let c = Column::new("1".to_owned(), ColumnType::Bool);
-        let t = TableSchema::new("0".to_owned(), vec![c]);
+        let t = TableSchema::new("0".to_owned(), vec![c], 1);
         let s = t.serialize();
         let expected: Vec<u8> = vec![
             1, //len of table name 
             48, //name
+            1,0,0,0, // page id 1 (u32) in LE
             1, //number of col
             1, //col type
             1, // len of col name
@@ -120,6 +135,7 @@ mod tests {
         let data: Vec<u8> = vec![
             1, //len of table name 
             48, //name
+            1,0,0,0, // page id 1 (u32) in LE
             1, //number of col
             1, //col type
             1, // len of col name
